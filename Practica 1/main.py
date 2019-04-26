9aca32721b6ff84ae8a206670163eef6a903c27c
leonardohra <leonardohra@gmail.com>
2019-04-26 15:06:20 -0300

    First commit, with the first practice file

---
+++ Practica 1/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Practice exercise for the Web Data Mining Class, UNICEN-Tandil, 2019.
+The program will: 
+- Download the 27 chapters of the book "The Little Prince" (http://www.angelfire.com/hi/littleprince/frames.html)
+- Find the stopwords for this text
+- Eliminate noise data, tokenize and normalize the text
+- Plot the graph for the dispersion of certain words
+{License_info}
+
+"""
+
+__author__ = 'Leonardo'
+__copyright__ = 'Copyright 2019, Mining the Little Prince'
+__credits__ = ['Leonardo Henrique da Rocha Araujo']
+__license__ = 'GNU GLPv3'
+__version__ = '0.1.0'
+__maintainer__ = 'Leonardo'
+__email__ = 'leonardo.araujo@isistan.unicen.edu.ar'
+__status__ = 'Dev'
+
+def main():
+	print("Start")
+
+if __name__ == "__main__":
+    main()
---
+++ python header.py
@@ -0,0 +1,63 @@
+#!interpreter [optional-arg]
+# -*- coding: utf-8 -*-
+
+"""
+{Description}
+{License_info}
+"""
+
+# Futures
+from __future__ import print_function
+# […]
+
+# Built-in/Generic Imports
+import os
+import sys
+# […]
+
+# Libs
+import pandas as pd # Or any other
+# […]
+
+# Own modules
+from {path} import {class}
+# […]
+
+__author__ = '{author}'
+__copyright__ = 'Copyright {year}, {project_name}'
+__credits__ = ['{credit_list}']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = '{maintainer}'
+__email__ = '{contact_email}'
+__status__ = '{dev_status}'
+
+
+
+
+
+# Futures
+from __future__ import unicode_literals
+from __future__ import print_function
+
+# Generic/Built-in
+import datetime
+import argparse
+
+# Other Libs
+import youtube_dl
+import pandas as pd
+
+# Owned
+from nostalgia_util import log_utils
+from nostalgia_util import settings_util
+__author__ = “Andrei Rukavina”
+__copyright__ = “Copyright 2017, The Nostalgic project”
+__credits__ = [“Andrei Rukavina”]
+__license__ = “MPL 2.0”
+__version__ = “0.1.0”
+__maintainer__ = “Andrei Rukavina”
+__email__ = “rukavina.andrei@gmail.com”
+__status__ = “Dev”
+
+# {code}
